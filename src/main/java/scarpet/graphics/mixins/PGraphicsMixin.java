01fd6e3608f3a8d9a95bf44a97577bc8fd0f2a36
replaceitem <40722305+replaceitem@users.noreply.github.com>
2022-07-31 23:44:07 +0200

    Fixed text_size

---
+++ src/main/java/scarpet/graphics/mixins/PGraphicsMixin.java
@@ -0,0 +1,15 @@
+package scarpet.graphics.mixins;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import processing.core.PApplet;
+import processing.core.PGraphics;
+
+@Mixin(PGraphics.class)
+public class PGraphicsMixin {
+    @Redirect(method = "createFont", at = @At(value = "FIELD",target = "Lprocessing/core/PApplet;pixelDensity:I"), remap = false)
+    private int defaultPixelDensity(PApplet instance) {
+        return 1;
+    }
+}
